598dc220b27e064194896074159cd93effdfd093
ivang187 <georgiev.ivan19997@gmail.com>
2024-03-29 14:59:56 +0200

    added claims principal and app builder extensions

--- TastyDelivery/Extensions/ApplicationBuilderExtension.cs
+++ TastyDelivery/Extensions/ApplicationBuilderExtension.cs
@@ -33,8 +33,7 @@ namespace TastyDelivery.Extensions
             }).GetAwaiter()
             .GetResult();
             
-            return app;
-                
+            return app;              
         }
     
     }
