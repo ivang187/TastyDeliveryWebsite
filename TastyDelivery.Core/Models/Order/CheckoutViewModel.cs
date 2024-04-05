8725c4a9e4749343f8184a422d1b8b77363b1ca1
ivang187 <georgiev.ivan19997@gmail.com>
2024-04-05 13:03:18 +0300

    created checkout full checkout page

---
+++ TastyDelivery.Core/Models/Order/CheckoutViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TastyDelivery.Core.Models.ShoppingCart;
+using TastyDelivery.Infrastructure.Data.Models;
+using TastyDelivery.Infrastructure.Data.Models.IdentityModels;
+
+namespace TastyDelivery.Core.Models.Order
+{
+    public class CheckoutViewModel
+    {
+        public ApplicationUser User { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public ICollection<CartItemViewModel> Products { get; set; } = new List<CartItemViewModel>();   
+    }
+}
--- TastyDelivery/Controllers/OrderController.cs
+++ TastyDelivery/Controllers/OrderController.cs
@@ -1,24 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Net;
+using System.Security.Claims;
+using TastyDelivery.Core.Models.Order;
 using TastyDelivery.Core.Models.ShoppingCart;
+using TastyDelivery.Infrastructure.Data.Models.IdentityModels;
 
 namespace TastyDelivery.Controllers
 {
     public class OrderController : Controller
     {
-        public IActionResult Checkout(string cartData)
+        private readonly UserManager<ApplicationUser> userManager;
+        
+        public OrderController(UserManager<ApplicationUser> _userManager) 
+        {
+            userManager = _userManager;
+        }
+        public async Task<IActionResult> Checkout(string cartData)
         {
             if (!String.IsNullOrEmpty(cartData))
             {
+                var user = await GetUser();
+
                 string decodedCartJson = WebUtility.UrlDecode(cartData);
 
                 var cart = JsonConvert.DeserializeObject<Cart>(decodedCartJson);
 
-                return View(cart);
+                var products = cart.Products.Select(p => new CartItemViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Price = p.Price,
+                    Quantity = p.Quantity,
+                }).ToList();
+
+                var order = new CheckoutViewModel
+                {
+                    Products = products,
+                    User = user,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                };
+
+                return View(order);
+
             }
 
             return View();
+        }
+
+        private async Task<ApplicationUser> GetUser()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (userId != null)
+            {
+                var user = await userManager.FindByIdAsync(userId);
+                return user;
+            }
+
+            return null;
         }
     }
 }
